c4b3ca82ad1cbbcaf9fa125ca8c8c8f00546b4b0
OmgSmileDeath <araragikyn@gmail.com>
2023-10-13 21:49:40 +0800

    added folders

--- asp_ver/Program.cs
+++ asp_ver/Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
+app.UseDeveloperExceptionPage();
+app.UseStatusCodePages();
+app.UseStaticFiles();
+app.UseRouting();
 
 app.MapGet("/", () => "Hello World!");
 
